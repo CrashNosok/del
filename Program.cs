25ef339e80c11d31dc2fd00dbf0ab4e4fbd49be8
CrashNosok <51856752+CrashNosok@users.noreply.github.com>
2020-05-23 19:40:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+
+    //написать свой собственный класс Exception
+    //добавить пару полей для более конкртетной информации(public)
+    // добавить пару методов(public)
+    // try/catch/catch/catch/catch/catch/finaly
+
+    class MyOwnException : Exception
+    {
+        public MyOwnException() : base("Это моё исключение!!")
+        {
+        }
+
+        public MyOwnException(string message) : base(message)
+        {
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // исключения. синтаксис
+            int i = 1;
+            try
+            {
+                //int i = 5;
+                //int j = i / 9;
+                //Console.WriteLine(j);
+
+                //int a = 2000000000;
+                //int b = 2000000000;
+                //int c = checked(a * b);
+                //Console.WriteLine(c);
+
+                //Exception ex = new Exception();
+                //throw new MyOwnException();
+                throw new ArgumentNullException("i", "qqq");
+
+            }
+            catch (MyOwnException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex) when (i == 5)
+            {
+                Console.WriteLine($"1) i = {i}");
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"2) i = {i}");
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("работа завершена");
+                Console.WriteLine(ex.Message);
+                
+            }
+            finally
+            {
+                Console.WriteLine("Работает finaly");
+            }
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
